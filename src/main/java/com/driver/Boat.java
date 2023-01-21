bf9514b4f101c79f2aa588159544d2e534ccee09
Gaurav Kumar <109330154+akashsInterra@users.noreply.github.com>
2023-01-21 19:20:18 +0530

    7 commit

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -11,16 +11,9 @@ public class Boat implements WaterVehicle{
 
 
 
-    public String getName() {
-        return name;
+    public void setName(String name) {
+        this.name=name;
     }
-
-
-
-    public int getCapacity() {
-        return capacity;
-    }
-
     public void setCapacity(int capacity) {
         this.capacity = capacity;
     }
